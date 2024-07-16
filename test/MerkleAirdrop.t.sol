eaf502f625ff3837d0be577e3f5b69434af2c854
Pranav <pranavlakkadi@gmail.com>
2024-07-16 10:28:31 +0530

    added the scripts

--- test/MerkleAirdrop.t.sol
+++ test/MerkleAirdrop.t.sol
@@ -2,7 +2,21 @@
 pragma solidity ^0.8.24;
 
 import {Test} from "forge-std/Test.sol";
+import {MerkleAirdrop} from "../src/Merkle-Airdrop.sol";
+import {PranavCoin} from "../src/PranavCoin.sol";
 
 contract MerkleAirdropTest is Test {
-    constructor() {}
+    MerkleAirdrop private airdrop;
+    PranavCoin private token;
+    bytes32 public ROOT = 0xaa5d581231e596618465a56aa0f5870ba6e20785fe436d5bfb82b08662ccc7c4;
+    address user;
+    uint256 userAmount;
+
+    function setUp() public {
+        token = new PranavCoin();
+        airdrop = new MerkleAirdrop(ROOT, token);
+        (user, userAmount) = makeAddrAndKey("user");
+    }
+
+    function testUsersCanClaim() public {}
 }
