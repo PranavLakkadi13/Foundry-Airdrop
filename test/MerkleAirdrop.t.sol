43314ce657cf3065efb623151892049254bb33df
Pranav <pranavlakkadi@gmail.com>
2024-07-16 22:34:52 +0530

    added the scripts

--- test/MerkleAirdrop.t.sol
+++ test/MerkleAirdrop.t.sol
@@ -1,22 +1,41 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
 
-import {Test} from "forge-std/Test.sol";
+import {Test,console} from "forge-std/Test.sol";
 import {MerkleAirdrop} from "../src/Merkle-Airdrop.sol";
 import {PranavCoin} from "../src/PranavCoin.sol";
+import {console} from "../lib/forge-std/src/console.sol";
 
 contract MerkleAirdropTest is Test {
     MerkleAirdrop private airdrop;
     PranavCoin private token;
+
     bytes32 public ROOT = 0xaa5d581231e596618465a56aa0f5870ba6e20785fe436d5bfb82b08662ccc7c4;
+
+    uint256 AmountToClaim = 25 * 1e18;
+    uint256 AmountToMint = AmountToClaim * 4;
+
+    bytes32 proofOne = 0x0fd7c981d39bece61f7499702bf59b3114a90e66b51ba2c53abdf7b62986c00a;
+    bytes32 proofTwo = 0xe5ebd1e1b5a5478a944ecab36a9a954ac3b6b8216875f6524caa7a1d87096576;
+    bytes32[] PROOF = [proofOne, proofTwo];
     address user;
-    uint256 userAmount;
+    uint256 userPrivKey;
+    address test = makeAddr("test");
 
     function setUp() public {
         token = new PranavCoin();
         airdrop = new MerkleAirdrop(ROOT, token);
-        (user, userAmount) = makeAddrAndKey("user");
+        token.mint(address(airdrop),AmountToMint );
+        (user, userPrivKey) = makeAddrAndKey("user");
     }
 
-    function testUsersCanClaim() public {}
+    function testUsersCanClaim() public {
+        uint256 startBalance = token.balanceOf(user);
+
+        vm.prank(user);
+        airdrop.claim(PROOF, user, AmountToClaim);
+
+        uint256 endBalance = token.balanceOf(user);
+        assert(endBalance == startBalance );
+    }
 }
