49b003d8a358887380f9b488c2a5fd13db44b1b5
Pranav <pranavlakkadi@gmail.com>
2024-07-20 22:37:59 +0530

    updated code

--- src/Merkle-Airdrop.sol
+++ src/Merkle-Airdrop.sol
@@ -5,6 +5,11 @@ import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import {MerkleProof} from "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
 
+/*
+@title MerkleAirdrop
+@notice This contract is used to distribute tokens to a list of addresses using Merkle Proofs
+@author Pranav Lakkadi
+*/
 contract MerkleAirdrop {
     //    List of addresses
     //    Only the address in the list can claim the tokens
@@ -16,6 +21,7 @@ contract MerkleAirdrop {
     //////////////////////////////////////////////
     error MerkleAirdrop__ClaimFailedInvalidProof();
     error MerkleAirdrop__AlreadyClaimed();
+    error  MerkleAirdrop__InvalidSignature();
 
     //////////////////////////////////////////////
     ///////////////// EVENTS /////////////////////
@@ -38,26 +44,6 @@ contract MerkleAirdrop {
     //////////// CORE FUNCTIONS //////////////////
     //////////////////////////////////////////////
 
-    function claimByPermit(bytes32[] calldata merkleProof, address account, uint256 amount) external {
-        //        using the account and the amount we can calculate the leaf node (hash)
-
-        if (s_claimed[account]) {
-            revert MerkleAirdrop__AlreadyClaimed();
-        }
-        //        here we are hashing the leaf twice to prevent it from pre image attack if ever their is another
-        //        leaf that produces the same hash , but anyhow keccak is secure enough to prevent this but we
-        //        are doing it as a practice that we follow
-        bytes32 leaf = keccak256(bytes.concat(keccak256(abi.encode(account, amount))));
-        if (!MerkleProof.verify(merkleProof, i_merkleRoot, leaf)) {
-            revert MerkleAirdrop__ClaimFailedInvalidProof();
-        }
-        s_claimed[account] = true;
-
-        emit AirdropClaim(account, amount);
-        SafeERC20.safeTransfer(i_token, account, amount);
-    }
-
-
     function claimSelf(bytes32[] calldata merkleProof, uint256 amount) external {
         //        using the account and the amount we can calculate the leaf node (hash)
 
@@ -76,6 +62,39 @@ contract MerkleAirdrop {
         emit AirdropClaim(msg.sender, amount);
         SafeERC20.safeTransfer(i_token, msg.sender, amount);
     }
+
+    function claimByPermit(
+        bytes32[] calldata merkleProof,
+        address account,
+        uint256 amount,
+        uint8 v,
+        bytes32 r,
+        bytes32 s
+    ) external {
+        if (s_claimed[account]) {
+            revert MerkleAirdrop__AlreadyClaimed();
+        }
+
+        // check the signature
+        if (!_isValidSignature(account, message, v, r, s)) {
+            revert MerkleAirdrop__InvalidSignature();
+        }
+
+        bytes32 leaf = keccak256(bytes.concat(keccak256(abi.encode(account, amount))));
+        if (!MerkleProof.verify(merkleProof, i_merkleRoot, leaf)) {
+            revert MerkleAirdrop__ClaimFailedInvalidProof();
+        }
+        s_claimed[account] = true;
+
+        emit AirdropClaim(account, amount);
+        SafeERC20.safeTransfer(i_token, account, amount);
+    }
+
+    //////////////////////////////////////////////
+    //////////// INTERNAL FUNCTIONS //////////////
+    //////////////////////////////////////////////
+
+//    function isValidSignature()
 
     //////////////////////////////////////////////
     //////////// GETTER FUNCTIONS ////////////////
